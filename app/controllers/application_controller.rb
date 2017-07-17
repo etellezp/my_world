328211efac01a98eeb329be80c8ff4d95595c760
github username <github email address>
2017-07-17 12:59:56 -0400

    Code Helpers Methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "my_world_secret"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_traveler
+    end
+
+    def current_traveler
+      @current_traveler ||= Traveler.find_by(id: session[:traveler_id]) if session[:traveler_id]
+    end
   end
 
 end
