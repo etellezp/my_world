32a8528325c313b6311b5b7a4662987d79278964
github username <github email address>
2017-07-18 10:22:22 -0400

    Update Controllers and Set up Views

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,73 @@
 class RecommendationsController < ApplicationController
+  get '/recommendations' do
+    if logged_in?
+      @recommendations = Recommendation.all
+      erb :'recommendations/recommendations'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/recommendations/new' do
+    if logged_in?
+      erb :'recommendations/create_recommendation'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/recommendations' do
+    if params[:location] == "" || params[:content] == "" || params[:rating] == ""
+      redirect to '/recommendations/new'
+    else
+      @recommendation = current_traveler.recommendations.create(params)
+      redirect to "/recommendations/#{@recommendation.id}"
+    end
+  end
+
+  get '/recommendations/:id' do
+    if logged_in?
+      @recommendation = Recommendation.find_by_id(params[:id])
+      erb :'recommendations/show_recommendation'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/recommendations/:id/edit' do
+    if logged_in?
+      @recommendation = current_traveler.recommendations.find_by(id: params[:id])
+      if @recommendation
+        erb :'recommendations/edit_recommendation'
+      else
+        redirect to '/recommendations'
+      end
+    end
+  end
+
+  patch '/recommendations/:id' do
+    if params[:location] == "" || params[:content] == "" || params[:rating] == ""
+      redirect to "/recommendations/#{params[:id]}/edit"
+    else
+      @recommendation = Recommendation.find_by_id(params[:id])
+      @recommendation.location = params[:location]
+      @recommendation.content = params[:content]
+      @recommendation.rating = params[:rating]
+      @recommendation.save
+      redirect to "/recommendations/#{@recommendation.id}"
+    end
+  end
+
+  delete '/recommendations/:id/delete' do
+    if logged_in?
+      recommendation = current_traveler.recommendations.find_by(params[:id])
+      if recommendation
+        recommendation.delete
+        redirect to '/recommendations'
+      else
+        redirect to '/recommendations'
+      end
+    end
+  end
+
 end
--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,2 +1,48 @@
 class TravelersController < ApplicationController
+
+  get '/signup' do
+    if !logged_in?
+      erb :'travelers/create_traveler'
+    else
+      redirect to '/recommendations'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @traveler = Traveler.new(username: params[:username], email: params[:email], password: params[:password])
+      @traveler.save
+      session[:traveler_id] = @traveler.id
+      redirect to '/recommendations'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'travelers/login'
+    else
+      redirect to '/recommendations'
+    end
+  end
+
+  post '/login' do
+    traveler = Traveler.find_by(username: params[:username])
+    if traveler && traveler.authenticate(params[:password])
+      session[:traveler_id] = traveler.id
+      redirect to '/recommendations'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
